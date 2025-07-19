ebf9bf539b307a3fea9b652de47039bc249b88be
PavankumarSN <pavansn675@gmail.com>
2025-07-19 23:14:46 +0530

    added first python project

--- Selenium_Project/Selenium/WebDriver_Basics.py
+++ Selenium_Project/Selenium/WebDriver_Basics.py
@@ -6,28 +6,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-# Configure logging
-logging.basicConfig(
-    filename='test_log.log',           # Optional: write logs to a file
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+if __name__ == "__main__":
+    # Configure logging
+    logging.basicConfig(
+        filename='test_log.log',           # Optional: write logs to a file
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
-logging.info("Starting test")
-driver = webdriver.Chrome()
-driver.get("https://www.python.org/")
-driver.maximize_window()
-print(f"The url is: {driver.title}")
-driver.implicitly_wait(5)
-search_bar = driver.find_element(By.XPATH, "//input[@type='search']")
-search_bar.clear()
-search_bar.send_keys("Getting started with Python")
-search_bar.send_keys(Keys.RETURN)
-driver.implicitly_wait(5)
-time.sleep(10)
-driver.quit()
-print("------------------------------------...")
-print("------------------------------------...")
-print("------------------------------------...")
-print("------------------------------------...")
-logging.info("Ending test")
+    logging.info("Starting test")
+    driver = webdriver.Chrome()
+    driver.get("https://www.python.org/")
+    driver.maximize_window()
+    print(f"The url is: {driver.title}")
+    driver.implicitly_wait(5)
+    search_bar = driver.find_element(By.XPATH, "//input[@type='search']")
+    search_bar.clear()
+    search_bar.send_keys("Getting started with Python")
+    search_bar.send_keys(Keys.RETURN)
+    driver.implicitly_wait(5)
+    time.sleep(10)
+    driver.quit()
+    print("------------------------------------...")
+    print("------------------------------------...")
+    print("------------------------------------...")
+    print("------------------------------------...")
+    logging.info("Ending test")
