42458b8cc2f9bb7d5ac484d90cdc47302d10690e
PavankumarSN <pavansn675@gmail.com>
2025-07-19 23:06:30 +0530

    added first python project

--- Selenium_Project/Selenium/WebDriver_Basics.py
+++ Selenium_Project/Selenium/WebDriver_Basics.py
@@ -1,12 +1,19 @@
 import time
-
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-print("Started execution  successfully...")
+# Configure logging
+logging.basicConfig(
+    filename='test_log.log',           # Optional: write logs to a file
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logging.info("Starting test")
 driver = webdriver.Chrome()
 driver.get("https://www.python.org/")
 driver.maximize_window()
@@ -23,4 +30,4 @@ print("------------------------------------...")
 print("------------------------------------...")
 print("------------------------------------...")
 print("------------------------------------...")
-print("Executed successfully...")
+logging.info("Ending test")
